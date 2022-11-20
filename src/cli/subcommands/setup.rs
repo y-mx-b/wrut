c9f82cf012ab0ba9acdd4e93bcfba65af773d445
Slope <matthewna1031@gmail.com>
2022-11-21 01:06:43 +0900

    Added individual overwrite options

--- src/cli/subcommands/setup.rs
+++ src/cli/subcommands/setup.rs
@@ -1,9 +1,32 @@
-use clap::Args;
+use clap::{Args, ValueEnum};
 
 /// Contains args for `setup` subcommand.
 #[derive(Args, Debug)]
 pub struct SetupArgs {
-    /// Overwrite existing configuration and data.
     #[clap(short, long)]
     pub force: bool,
+    #[clap(short, long, value_delimiter = ',')] 
+    pub overwrite: Vec<SetupOverwrite>
+}
+
+#[derive(ValueEnum, Debug, PartialEq, Eq, Clone)]
+pub enum SetupOverwrite {
+    #[clap(alias = "a")]
+    All,
+
+    // Directories
+    #[clap(alias = "d")]
+    Data,
+    #[clap(alias = "o")]
+    Obj,
+    #[clap(alias = "p")]
+    Projects,
+    #[clap(alias = "t")]
+    Templates,
+    #[clap(alias = "s")]
+    Tags,
+
+    // Files
+    #[clap(alias = "c")]
+    Config,
 }
