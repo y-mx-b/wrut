a16436a044b4268807b7eda4c68974c76a220c1f
Slope <linearfunc@proton.me>
2023-01-13 22:22:50 +0900

    Added individual overwrite options

--- src/backend/error.rs
+++ src/backend/error.rs
@@ -1,3 +1,4 @@
+use crate::cli::subcommands::SetupOverwrite;
 use std::path::PathBuf;
 use thiserror::Error;
 
@@ -10,6 +11,8 @@ pub enum WutError {
     // Setup errors
     #[error("{0:?} already exist(s) but the `force` option was not set.")]
     SetupDirAlreadyExists(Vec<PathBuf>),
+    #[error("{0:?} already eixst(s) but the `{1:?}` option was not set.")]
+    MissingOverwriteFlag(Vec<PathBuf>, SetupOverwrite),
 
     // Init errors
     #[error("Failed to acquire the name of the directory at {0:?}.")]
