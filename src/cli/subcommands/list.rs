fc73ffa968c288f2749a06186230d719bf146849
Slope <linearfunc@proton.me>
2023-01-13 22:22:49 +0900

    Renamed arg structs

--- src/cli/subcommands/list.rs
+++ src/cli/subcommands/list.rs
@@ -2,7 +2,7 @@ use crate::cli::Type;
 use clap::Args;
 
 #[derive(Args, Debug)]
-pub struct List {
+pub struct ListArgs {
     #[clap(value_enum, default_value_t = Type::Project)]
     type_: Type,
 }
