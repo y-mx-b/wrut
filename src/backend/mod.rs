1ad5b59b3db771962c51c7dd432b581604e7ebce
Slope <linearfunc@proton.me>
2023-01-13 22:22:49 +0900

    Reorganized backend setup code

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,7 +1,5 @@
 pub mod comp;
-mod r#const;
 mod error;
 pub mod setup;
 
 pub use error::*;
-pub use r#const::*;
--- src/backend/setup.rs
+++ src/backend/setup.rs
@@ -1,12 +1,37 @@
-use crate::backend::{Dirs, WutError};
+use crate::backend::WutError;
 use crate::cli::subcommands::SetupArgs;
 use anyhow::{Context, Result};
+use home::home_dir;
 use log::info;
+use std::collections::HashMap;
 use std::fs;
+use std::path::PathBuf;
+
+/// Each variant refers to a specific directory required for `wut` to function
+#[derive(Hash, Eq, PartialEq, Debug)]
+pub enum Dirs {
+    Config,
+    Projects,
+    Tags,
+    Templates,
+    Obj,
+}
+
+/// Returns a `HashMap` of directory paths mapped to `Dirs` variants
+pub fn dirs() -> Result<HashMap<Dirs, PathBuf>, WutError> {
+    let home = home_dir().ok_or(WutError::HomeDirectoryNotFound)?;
+    Ok(HashMap::from([
+        (Dirs::Config, home.join(".config/wut")),
+        (Dirs::Projects, home.join(".wut/projects")),
+        (Dirs::Tags, home.join(".wut/tags")),
+        (Dirs::Templates, home.join(".wut/templates")),
+        (Dirs::Obj, home.join(".wut/.obj")),
+    ]))
+}
 
 /// Initializes all prerequisites for `wut` to function
 pub fn setup(args: &SetupArgs) -> Result<()> {
-    let dirs = Dirs::dirs()?;
+    let dirs = dirs()?;
 
     // delete and create all dirs if `force` is set
     if args.force {
