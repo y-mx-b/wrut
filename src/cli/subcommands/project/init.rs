c4f948bf4963c82bff1c4ce408f41655008490a1
Slope <linearfunc@proton.me>
2023-01-13 22:22:50 +0900

    Added project init command

--- src/cli/subcommands/project/init.rs
+++ src/cli/subcommands/project/init.rs
@@ -1,6 +1,15 @@
-use clap::Args;
+use crate::cli::util::get_values;
+use clap::{builder::PossibleValuesParser, Args};
+use crate::cli::Type;
 
 #[derive(Args, Debug)]
 pub struct InitArgs {
-
+    /// The template to initialize a project from.
+    #[clap(long, short, hide_possible_values = true)]
+    #[clap(value_parser = PossibleValuesParser::new(get_values(Type::Template)))]
+    pub template: String,
+    /// The name of the project to initialize.
+    ///
+    /// By default, the name of the current directory will be used.
+    pub name: Option<String>
 }
