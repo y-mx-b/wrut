849eb98aab0501817c9b42bab668450e446b029e
Slope <linearfunc@proton.me>
2023-01-13 22:22:49 +0900

    Added template/project registering

--- src/cli/subcommands/init.rs
+++ src/cli/subcommands/init.rs
@@ -4,10 +4,12 @@ use clap::{Args, ValueEnum};
 pub struct InitArgs {
     /// The type of directory to initialize
     #[clap(value_enum, value_name = "TYPE", default_value_t = InitType::Project)]
-    type_: InitType,
+    pub type_: InitType,
+    #[clap(long, short)]
+    pub name: Option<String>,
 }
 
-#[derive(ValueEnum, Debug, PartialEq, Eq, Clone)]
+#[derive(ValueEnum, Debug, PartialEq, Eq, Clone, Copy)]
 pub enum InitType {
     Project,
     Template,
