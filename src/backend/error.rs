8448b48d47a41150f02098e68c3f346cb3b2dc50
Slope <linearfunc@proton.me>
2023-01-13 22:22:50 +0900

    Updated config functions

--- src/backend/error.rs
+++ src/backend/error.rs
@@ -4,14 +4,18 @@ use thiserror::Error;
 #[derive(Error, Debug)]
 pub enum WutError {
     // IO related errors
-    #[error("Could not find home directory")]
+    #[error("Could not find home directory.")]
     HomeDirectoryNotFound,
 
     // Setup errors
-    #[error("{0:?} already exist(s) but the `force` option was not set")]
+    #[error("{0:?} already exist(s) but the `force` option was not set.")]
     SetupDirAlreadyExists(Vec<PathBuf>),
 
     // Init errors
     #[error("Failed to acquire the name of the directory at {0:?}.")]
     FailedToAcquireDirectoryName(PathBuf),
+
+    // Config errors
+    #[error("Failed to read configuration file at {0:?}.")]
+    FailedToReadConfigFile(PathBuf),
 }
