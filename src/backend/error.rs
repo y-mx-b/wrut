14c288a19b7d45450889cea42fc44ae715ee0df4
Slope <linearfunc@proton.me>
2023-01-13 22:22:49 +0900

    Improved error handling

--- src/backend/error.rs
+++ src/backend/error.rs
@@ -6,8 +6,6 @@ pub enum WutError {
     // IO related errors
     #[error("Could not find home directory")]
     HomeDirectoryNotFound,
-    #[error("{0:?}")]
-    Io(#[from] std::io::Error),
 
     // Init errors
     #[error("{0:?} already exist(s) but the `force` option was not set")]
